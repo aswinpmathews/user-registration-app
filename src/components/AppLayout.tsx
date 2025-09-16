5fd66b27f1b26226c58004d9e5fb12030c3c85ac
Aswin P Mathews <aswinpmathews@gmail.com>
2025-09-16 13:21:56 +0530

    style and route separation

---
+++ src/components/AppLayout.tsx
@@ -0,0 +1,30 @@
+import { CssVarsProvider } from '@mui/joy/styles';
+import Sheet from '@mui/joy/Sheet';
+import { ReactNode } from 'react';
+
+type AppLayoutProps = {
+  children: ReactNode;
+};
+
+export default function AppLayout({ children }: AppLayoutProps) {
+  return (
+    <CssVarsProvider>
+      <Sheet
+        sx={{
+          width: 1000,
+          mx: 'auto',
+          my: 4,
+          py: 3,
+          px: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 6,
+          borderRadius: 'sm',
+          boxShadow: 'md',
+        }}
+      >
+        {children}
+      </Sheet>
+    </CssVarsProvider>
+  );
+}
